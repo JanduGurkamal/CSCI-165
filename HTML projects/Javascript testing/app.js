b734b21929bc8fa1feb99814b7a4e221e7549287
Gurkamal Singh Jandu <134562899+JanduGurkamal@users.noreply.github.com>
2024-06-25 16:10:08 -0700

    Complete Class Quiz

--- HTML projects/Javascript testing/app.js
+++ HTML projects/Javascript testing/app.js
@@ -1,3 +1,13 @@
-console.log("Welcome to Git repo");
-let message = prompt("What is your full name");
-document.write(`Welcome ${message}`)
+let a=10;
+{
+console.log(a);
+const b = 12;
+console.log(b);
+}
+{
+console.log(a);
+const c = 30;
+console.log(c);
+}
+console.log(b);
+console.log(c);
